0ea126bae35b7e556ac122aee214dd3213ae9572
JP Dillingham <jpdillingham@gmail.com>
2019-03-17 16:53:24 -0500

    tweak dotnet test

--- tools/test.sh
+++ tools/test.sh
@@ -1,1 +1,1 @@
-dotnet test -p:CollectCoverage=true -p:CoverletOutput='./coverage.xml' -p:CoverletOutputFormat=opencover tests/Utility.CommandLine.Progress.Tests /p:Exclude='[xunit*]*'
+dotnet test -p:CollectCoverage=true -p:CoverletOutput="./coverage.xml" -p:CoverletOutputFormat=opencover tests/Utility.CommandLine.Progress.Tests -p:Include="[Utility.CommandLine.Progress*]*" -p:Exclude="[*.Tests]*"
