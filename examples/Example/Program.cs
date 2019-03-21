4f5a31ecb39691cfb1111b5b1983e30ca8ae2387
JP Dillingham <jpdillingham@gmail.com>
2019-03-20 21:14:49 -0500

    fix examples

--- examples/Example/Program.cs
+++ examples/Example/Program.cs
@@ -22,7 +22,7 @@ namespace Example
         {
             for (int i = 0; i < pb.Maximum; i++)
             {
-                pb.PerformStep();
+                pb.Increment();
                 action(pb, ps, m);
                 //Console.Write("\n");
                 Thread.Sleep(25);
@@ -37,7 +37,7 @@ namespace Example
 
             for (int i = 0; i < 100; i++)
             {
-                pb.PerformStep();
+                pb.Increment();
                 Console.Write($"\r{pb}");
                 Thread.Sleep(10);
             }
@@ -53,7 +53,7 @@ namespace Example
 
             for (int i = 0; i < 100; i++)
             {
-                pb.PerformStep();
+                pb.Increment();
                 Console.Write($"\r{text} [{Math.Round(pb.Percent * 100).ToString().PadLeft(3)}%] {pb}");
                 Thread.Sleep(5);
             }
@@ -68,7 +68,7 @@ namespace Example
 
         static void Disappear()
         {
-            Loop(new ProgressBar(10, format: new ProgressBarFormat(full: '=', tip: '>', paddingLeft: 1, hideWhenComplete: true)), null, null, (pb, ps, _) => Console.Write($"\rDoing something...{pb} Some other text.".PadRight(Console.WindowWidth - 1)));
+            Loop(new ProgressBar(10, format: new ProgressBarFormat(full: '=', tip: '>', paddingLeft: 1)), null, null, (pb, ps, _) => Console.Write($"\rDoing something...{(pb.Complete ? pb.ToString() : string.Empty)} Some other text.".PadRight(Console.WindowWidth - 1)));
         }
 
         static void WithSpinner()
@@ -85,7 +85,7 @@ namespace Example
         {
             Loop(new ProgressBar(-30, format: new ProgressBarFormat(full: '█', empty: '░')), new Spinner('▀', '▄'), null, (pb, ps, _) => 
             {
-                ps.PerformStep();
+                ps.Spin();
                 Console.Write($"\rDoing something... {pb} {ps}".PadRight(Console.WindowWidth - 1));
             });
         }
@@ -94,8 +94,8 @@ namespace Example
         {
             Loop(new ProgressBar(50, 0, 500, 1, 0), new Spinner(), new Marquee("█▓▒░", 20, new MarqueeFormat(leftToRight: false, bounce: true, reverseTextOnBounce: true, gap: null)), (pb, ps, m) =>
             {
-                m.PerformStep();
-                ps.PerformStep();
+                m.Scroll();
+                ps.Spin();
                 Console.Write($"\r{pb} {m} {ps}");
             });
             //Loop(new ProgressBar(10, 0, 1000, 1, 0), null, new Marquee("Hello, World!", 30, new MarqueeFormat(leftToRight: false, bounce: true, reverseTextOnBounce: false, gap:0)), (pb, ps, m) =>
