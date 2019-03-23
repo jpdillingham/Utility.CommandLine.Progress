fd01da06deeb4c07b3f88e3deaf6259fec44ed01
JP Dillingham <jpdillingham@gmail.com>
2019-03-23 11:43:51 -0500

    add test for SpinnerFormat

---
+++ tests/Utility.CommandLine.Progress.Tests/ProgressTests.cs
@@ -0,0 +1,68 @@
+﻿/*
+  █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ▀▀▀  ▀  ▀      ▀▀
+  █  The MIT License (MIT)
+  █
+  █  Copyright (c) 2019 JP Dillingham (jp@dillingham.ws)
+  █
+  █  Permission is hereby granted, free of charge, to any person obtaining a copy
+  █  of this software and associated documentation files (the "Software"), to deal
+  █  in the Software without restriction, including without limitation the rights
+  █  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  █  copies of the Software, and to permit persons to whom the Software is
+  █  furnished to do so, subject to the following conditions:
+  █
+  █  The above copyright notice and this permission notice shall be included in all
+  █  copies or substantial portions of the Software.
+  █
+  █  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  █  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  █  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  █  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  █  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  █  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  █  SOFTWARE.
+  █
+  ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀  ▀▀ ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀██
+                                                                                               ██
+                                                                                           ▀█▄ ██ ▄█▀
+                                                                                             ▀████▀
+                                                                                               ▀▀                            */
+
+namespace Utility.CommandLine.ProgressBar.Tests
+{
+    using AutoFixture.Xunit2;
+    using System;
+    using Utility.CommandLine.Progress;
+    using Xunit;
+
+    public class ProgressTests
+    {
+        [Fact(DisplayName = "Meta test")]
+        public void Meta_Test()
+        {
+            Assert.True(true);
+        }
+
+        [Theory(DisplayName = "SpinnerFormat instantiates with the given values"), AutoData]
+        public void SpinnerFormat_Instantiates_With_The_Given_Values(char empty, string left, string right, int paddingLeft, int paddingRight, char pad)
+        {
+            Func<bool> e = () => true;
+            Func<bool> h = () => false;
+
+            SpinnerFormat f = null;
+            var ex = Record.Exception(() => f = new SpinnerFormat(empty, left, right, paddingLeft, paddingRight, pad, e, h));
+
+            Assert.Null(ex);
+
+            Assert.Equal(empty, f.Empty);
+            Assert.Equal(left, f.Left);
+            Assert.Equal(right, f.Right);
+            Assert.Equal(paddingLeft, f.PaddingLeft);
+            Assert.Equal(paddingRight, f.PaddingRight);
+            Assert.Equal(pad, f.Pad);
+
+            Assert.Equal(e, f.EmptyWhen);
+            Assert.Equal(h, f.HiddenWhen);
+        }
+    }
+}
