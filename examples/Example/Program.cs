f50b6f58b2d5dbfbe5d384903003dbfa9217987d
JP Dillingham <jpdillingham@gmail.com>
2019-03-24 16:25:03 -0500

    tweak examples again

--- examples/Example/Program.cs
+++ examples/Example/Program.cs
@@ -21,82 +21,82 @@ namespace Example
                 Console.Write(fullWidth);
             });
 
-            //var fixedWidth = new ProgressBar(20, format: new ProgressBarFormat(left: "[", right: "]", full: '=', tip: '>', empty: ' '));
-            //lines.Add(() =>
-            //{
-            //    fixedWidth.Increment();
-            //    Console.Write($"Here's a fixed width example: {fixedWidth}");
-            //});
+            var fixedWidth = new ProgressBar(20, format: new ProgressBarFormat(left: "[", right: "]", full: '=', tip: '>', empty: ' '));
+            lines.Add(() =>
+            {
+                fixedWidth.Increment();
+                Console.Write($"Here's a fixed width example: {fixedWidth}");
+            });
 
-            //var fixedWidthEmpty = new ProgressBar(20, format: new ProgressBarFormat(left: "[", right: "]", full: '=', tip: '>', empty: ' ', emptyWhen: () => fixedWidth.Complete));
-            //lines.Add(() =>
-            //{
-            //    fixedWidthEmpty.Increment();
-            //    Console.Write($"Here's one that appears empty when complete: {fixedWidthEmpty}!");
-            //});
+            var fixedWidthEmpty = new ProgressBar(20, format: new ProgressBarFormat(left: "[", right: "]", full: '=', tip: '>', empty: ' ', emptyWhen: () => fixedWidth.Complete));
+            lines.Add(() =>
+            {
+                fixedWidthEmpty.Increment();
+                Console.Write($"Here's one that appears empty when complete: {fixedWidthEmpty}!");
+            });
 
-            //var fixedWidthHidden = new ProgressBar(20, format: new ProgressBarFormat(left: "[", right: "]", full: '=', tip: '>', empty: ' ', hiddenWhen: () => fixedWidth.Complete));
-            //lines.Add(() =>
-            //{
-            //    fixedWidthHidden.Increment();
-            //    Console.Write($"Here's one that is hidden when complete: {fixedWidthHidden}");
-            //});
+            var fixedWidthHidden = new ProgressBar(20, format: new ProgressBarFormat(left: "[", right: "]", full: '=', tip: '>', empty: ' ', hiddenWhen: () => fixedWidth.Complete));
+            lines.Add(() =>
+            {
+                fixedWidthHidden.Increment();
+                Console.Write($"Here's one that is hidden when complete: {fixedWidthHidden}!".PadRight(Console.WindowWidth - 1));
+            });
 
-            //var fitToWidth = new ProgressBar(format: new ProgressBarFormat(left: "Here's one that fits to width: "));
-            //lines.Add(() =>
-            //{
-            //    fitToWidth.Increment();
-            //    Console.Write(fitToWidth);
-            //});
+            var fitToWidth = new ProgressBar(format: new ProgressBarFormat(left: "Here's one that fits to width: "));
+            lines.Add(() =>
+            {
+                fitToWidth.Increment();
+                Console.Write(fitToWidth);
+            });
 
-            //var withPercentage = new ProgressBar(width: -8, format: new ProgressBarFormat(left: "Here's one that shows percentage: "));
-            //lines.Add(() =>
-            //{
-            //    withPercentage.Increment();
-            //    Console.Write($"{withPercentage} [{(withPercentage.Percent * 100).ToString().PadLeft(3)}%]".PadRight(Console.WindowWidth - 1));
-            //});
+            var withPercentage = new ProgressBar(width: -8, format: new ProgressBarFormat(left: "Here's one that shows percentage: "));
+            lines.Add(() =>
+            {
+                withPercentage.Increment();
+                Console.Write($"{withPercentage} [{(withPercentage.Percent * 100).ToString().PadLeft(3)}%]".PadRight(Console.WindowWidth - 1));
+            });
 
-            //var helloWorld = new Marquee("Hello, World!", 15);
-            //lines.Add(() =>
-            //{
-            //    helloWorld.Scroll();
-            //    Console.Write($"Here's a marquee: {helloWorld}");
-            //});
+            var helloWorld = new Marquee("Hello, World!", 15);
+            lines.Add(() =>
+            {
+                helloWorld.Scroll();
+                Console.Write($"Here's a marquee: {helloWorld}");
+            });
 
-            //var helloWorld2 = new Marquee("Hello, World!", 15, new MarqueeFormat(leftToRight: true));
-            //lines.Add(() =>
-            //{
-            //    helloWorld2.Scroll();
-            //    Console.Write($"Here's one that goes the other way: {helloWorld2}");
-            //});
+            var helloWorld2 = new Marquee("Hello, World!", 15, new MarqueeFormat(leftToRight: true));
+            lines.Add(() =>
+            {
+                helloWorld2.Scroll();
+                Console.Write($"Here's one that goes the other way: {helloWorld2}");
+            });
 
-            //var helloWorld3 = new Marquee("Hello, World!", 15, new MarqueeFormat(bounce: true));
-            //lines.Add(() =>
-            //{
-            //    helloWorld3.Scroll();
-            //    Console.Write($"Here's one bounces: {helloWorld3}");
-            //});
+            var helloWorld3 = new Marquee("Hello, World!", 15, new MarqueeFormat(bounce: true));
+            lines.Add(() =>
+            {
+                helloWorld3.Scroll();
+                Console.Write($"Here's one bounces: {helloWorld3}");
+            });
 
-            //var knightRider = new Marquee("█▓▒░", 15, new MarqueeFormat(bounce: true, reverseTextOnBounce: true));
-            //lines.Add(() =>
-            //{
-            //    knightRider.Scroll();
-            //    Console.Write($"Here's one like Knight Rider: {knightRider}");
-            //});
+            var knightRider = new Marquee("█▓▒░", 15, new MarqueeFormat(bounce: true, reverseTextOnBounce: true));
+            lines.Add(() =>
+            {
+                knightRider.Scroll();
+                Console.Write($"Here's one like Knight Rider: {knightRider}");
+            });
 
-            //var slashes = new Marquee("/", 15, new MarqueeFormat(gap: 1));
-            //lines.Add(() =>
-            //{
-            //    slashes.Scroll();
-            //    Console.Write($"Here's one with a small gap: {slashes}");
-            //});
+            var slashes = new Marquee("/", 15, new MarqueeFormat(gap: 1));
+            lines.Add(() =>
+            {
+                slashes.Scroll();
+                Console.Write($"Here's one with a small gap: {slashes}");
+            });
 
-            //var doingDone = new Marquee("Doing a thing...", 15, new MarqueeFormat(gap: 1, left: "[", right: "]", complete: "Done!", completeWhen: () => fullWidth.Complete));
-            //lines.Add(() =>
-            //{
-            //    doingDone.Scroll();
-            //    Console.Write($"Here's one that changes when complete: {doingDone}");
-            //});
+            var doingDone = new Marquee("Doing a thing...", 15, new MarqueeFormat(gap: 1, left: "[", right: "]", complete: "Done!", completeWhen: () => fullWidth.Complete));
+            lines.Add(() =>
+            {
+                doingDone.Scroll();
+                Console.Write($"Here's one that changes when complete: {doingDone}");
+            });
 
             for (int i = 0; i < lines.Count; i++) { Console.WriteLine(); }
 
