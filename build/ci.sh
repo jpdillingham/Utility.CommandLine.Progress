76db15c55954a5f1fde4b538432babe11cbc161e
JP Dillingham <jpdillingham@gmail.com>
2019-04-07 12:14:13 -0500

    add sonarscanner to ci.sh

--- build/ci.sh
+++ build/ci.sh
@@ -2,7 +2,11 @@
 set -e
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+dotnet-sonarscanner begin /key:"jpdillingham_Utility.CommandLine.Progress" /o:jpdillingham-github /d:sonar.host.url="https://sonarcloud.io" /d:sonar.exclusions="**/*examples*/**" /d:sonar.login="${TOKEN_SONARCLOUD}" /d:sonar.cs.opencover.reportsPaths="tests/opencover.xml"
+
 . "${__dir}/build.sh"
 . "${__dir}/test.sh"
+
+dotnet-sonarscanner end /d:sonar.login="${TOKEN_SONARCLOUD}"
 
 bash <(curl -s https://codecov.io/bash) -f tests/opencover.xml
